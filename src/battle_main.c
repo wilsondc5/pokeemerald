bd27772706b11a7ee87265d77f44477b63c33665
TheXaman <48356183+TheXaman@users.noreply.github.com>
2022-04-17 17:09:45 +0200

    daasdasa

--- src/battle_main.c
+++ src/battle_main.c
@@ -65,10 +65,10 @@
 #include "overworld.h"
 
 #ifdef GBA_PRINTF
-    //#include "printf.h"
-    //#include "mgba.h"
-    //#include "data.h"                 // for gSpeciesNames, which maps species number to species name.
-    //#include "../gflib/string_util.h" // for ConvertToAscii()
+    #include "printf.h"
+    #include "mgba.h"
+    #include "data.h"                 // for gSpeciesNames, which maps species number to species name.
+    #include "../gflib/string_util.h" // for ConvertToAscii()
 #endif
 
 extern const struct BgTemplate gBattleBgTemplates[];
